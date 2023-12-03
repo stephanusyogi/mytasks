2fe732737b8ebd8db3baa388404b1e89a99b168b
stephanusyogi <stephanusyogi12@gmail.com>
2023-12-03 18:15:20 +0700

    Package

--- app/utils/connect.ts
+++ app/utils/connect.ts
@@ -1,22 +1,17 @@
-// import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from "@prisma/client";
 
-// let prisma : PrismaClient
+let prisma : PrismaClient
 
-// if (process.env.NODE_ENV === 'production') {
-//   prisma = new PrismaClient()
-// }else{
-//   //@ts-ignore
-//   if (!global.prisma) {
-//   //@ts-ignore
-//     global.prisma = new PrismaClient()
-//   }
-//   //@ts-ignore
-//   prisma = global.prisma
-// }
-
-import { PrismaClient } from '@prisma/client/edge'
-
-const prisma = new PrismaClient()
-
+if (process.env.NODE_ENV === 'production') {
+  prisma = new PrismaClient()
+}else{
+  //@ts-ignore
+  if (!global.prisma) {
+  //@ts-ignore
+    global.prisma = new PrismaClient()
+  }
+  //@ts-ignore
+  prisma = global.prisma
+}
 
 export default prisma
