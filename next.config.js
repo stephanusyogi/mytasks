e42b1b61c268f6c397898b32ed0dc175c67c7982
stephanusyogi <stephanusyogi12@gmail.com>
2023-12-03 22:29:43 +0700

    Updated Next Config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images:{
-    domains: ['localhost', 'https://img.clerk.com','img.clerk.com']
+    domains: ['localhost', 'https://img.clerk.com', 'img.clerk.com', 'https://mytasks-stephanusyogi.vercel.app', 'mytasks-stephanusyogi.vercel.app']
   }    
 }
 
